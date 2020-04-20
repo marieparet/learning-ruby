221f7c3ee4e24b31b161153a0c25f2f2e83b01df
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2020-04-20 11:18:37 +0200

    First commit

---
+++ 1.rb
@@ -0,0 +1,23 @@
+
+for nombre_a_afficher in 0..10 do
+    puts nombre_a_afficher
+    puts "Hello" if nombre_a_afficher == 5
+    puts "Coucou" if nombre_a_afficher == 6
+end
+
+
+
+
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# Hello
+# 6
+# Coucou
+# 7
+# 8
+# 9
+# 10
---
+++ 2.rb
@@ -0,0 +1,13 @@
+name = ARGV[0]
+
+list_prenoms_autorises = ["Michael", "Marie"]
+password = ARGV[1]
+
+if list_prenoms_autorises.include?(name) && password == "azerty"
+    puts "Hello Administrator #{name}"
+else
+    puts 'Casse toi connard'
+end
+
+
+
